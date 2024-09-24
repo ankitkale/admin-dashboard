fd298b85861c9f4352cbd17d8bb66786ac5653e4
ankitkale <91000714+ankitkale@users.noreply.github.com>
2024-09-24 12:00:02 +0530

    Analytics Page and Components

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -1,4 +1,10 @@
+import ChannelPerformance from "../components/analytics/ChannelPerformance";
 import OverviewCards from "../components/analytics/OverviewCards";
+import ProductPerformance from "../components/analytics/ProductPerformance";
+import RevenueChart from "../components/analytics/RevenueChart";
+import UserRetention from "../components/analytics/UserRetention";
+import CustomerSegmentation from "../components/analytics/CustomerSegmentation";
+import AIPoweredInsights from "../components/analytics/AIPoweredInsights";
 import Header from "../components/common/Header";
 
 
@@ -9,9 +15,14 @@ const AnalyticsPage = () => {
 
       <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'> 
         < OverviewCards />
+        <RevenueChart />
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8'>
-
+          <ChannelPerformance />
+          <ProductPerformance />
+          <UserRetention />
+          <CustomerSegmentation />
         </div>
+        <AIPoweredInsights />
       </main>
     </div>
   )
